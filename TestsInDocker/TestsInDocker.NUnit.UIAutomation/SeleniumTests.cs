98c371cd96975b4d52647d87955f9a29de44c43c
Bob Ansari <babakansari@hotmail.com>
2021-08-09 17:38:50 -0400

    Add UI Automation test

---
+++ TestsInDocker/TestsInDocker.NUnit.UIAutomation/SeleniumTests.cs
@@ -0,0 +1,39 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestsOnContainers
+{
+    [TestFixture]
+    public class SeleniumTests
+    {
+        private ChromeDriver _driver;
+        public TestContext TestContext { get; set; }
+
+        public static string CurrentAssemblyPath => TestContext.CurrentContext.TestDirectory;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+
+            Environment.SetEnvironmentVariable("webdriver.chrome.driver", @"/usr/bin/chromedriver");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            // chromeOptions.AddArguments("--headless");
+            chromeOptions.AddArguments("--no-sandbox");
+
+            _driver = new ChromeDriver(chromeOptions);
+
+            Console.WriteLine(@">>>>>>>>>>>>   /usr/bin/chromedriver <<<<<<<<<<<<<<<");
+        }
+
+        [Test]
+        public void PageLive_Test()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://www.google.ca");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            Assert.IsTrue(_driver.PageSource.Contains("About"));
+        }
+    }
+}
