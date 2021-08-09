63337e8ebdb39d0fec31bea0fc2670a9a4df300f
Bob Ansari <babakansari@hotmail.com>
2021-08-09 14:11:55 -0400

    Make power shell scripts more convenient to use

---
+++ TestsInDocker/PublichToUbuntu.ps1
@@ -0,0 +1,2 @@
+dotnet publish TestsInDocker -c Release --force --self-contained --runtime ubuntu.16.04-x64
+dotnet publish TestsInDocker.MSTest -c Release --force --self-contained --runtime ubuntu.16.04-x64
--- TestsInDocker/TestsInDocker.MSTest/PublichToUbuntu.ps1
+++
@@ -1,1 +0,0 @@
-dotnet publish -c Release --force --self-contained --runtime ubuntu.16.04-x64
