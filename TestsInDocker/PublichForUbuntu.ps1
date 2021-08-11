6267bdcf059f495f3b2cac48e21623f9b939e661
Bob Ansari <babakansari@hotmail.com>
2021-08-11 16:17:34 -0400

    Resolve Newtonsoft.Json.dll version 9.0.1 conflict with 13.0.1 between projects

--- TestsInDocker/PublichForUbuntu.ps1
+++ TestsInDocker/PublichForUbuntu.ps1
@@ -1,4 +1,5 @@
-dotnet publish TestsInDocker -c Release --force --no-self-contained --runtime ubuntu.16.04-x64
-dotnet publish TestsInDocker.MSTest -c Release --force --no-self-contained --runtime ubuntu.16.04-x64
-dotnet publish TestsInDocker.NUnit -c Release --force --no-self-contained --runtime ubuntu.16.04-x64
-dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --force --no-self-contained --runtime ubuntu.16.04-x64
+dotnet publish TestsInDocker -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/Console
+dotnet publish TestsInDocker.MSTest -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/MSTest
+dotnet publish TestsInDocker.NUnit -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/NUnit
+dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/UIAutomation
+dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --no-self-contained --runtime win-x64 --output Published/Win
