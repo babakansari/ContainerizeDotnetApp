a557ccdee464c8778551d2d128347e6d6ff61ee4
Bob Ansari <babakansari@hotmail.com>
2021-08-14 15:12:02 -0400

    Publish all the assemblies into one folder on host instead of separate one for each

--- TestsInDocker/PublichForUbuntu.ps1
+++ TestsInDocker/PublichForUbuntu.ps1
@@ -1,5 +1,5 @@
-dotnet publish TestsInDocker -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/Console
-dotnet publish TestsInDocker.MSTest -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/MSTest
-dotnet publish TestsInDocker.NUnit -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/NUnit
-dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published/UIAutomation
-dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --no-self-contained --runtime win-x64 --output Published/Win
+dotnet publish TestsInDocker -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published
+dotnet publish TestsInDocker.MSTest -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published
+dotnet publish TestsInDocker.NUnit -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published
+dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --no-self-contained --runtime ubuntu.16.04-x64 --output Published
+dotnet publish TestsInDocker.NUnit.UIAutomation -c Release --no-self-contained --runtime win-x64 --output Published
