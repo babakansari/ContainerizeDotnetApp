998d5da7976329b40406d80f619a196e8b8bb482
Bob Ansari <babakansari@hotmail.com>
2021-08-19 22:03:41 -0400

    Reference the binaries in the host instead of copying to the image

--- TestsInDocker/RunContainer.ps1
+++ TestsInDocker/RunContainer.ps1
@@ -1,2 +1,2 @@
-docker run -d -p 4444:4444 -p 5900:5900 --volume=${PWD}/publish1:/usr/src testsindocker
+docker run -d -p 4444:4444 -p 5900:5900  --mount type=bind,source=${PWD}\Published,target=/usr/src testsindocker
 
